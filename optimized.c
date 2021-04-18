f76b4e2b0ff995f77c94a0f8aa1a2050cea3eb34
fourtf <tf.four@gmail.com>
2021-04-18 21:35:03 +0200

    more AVX stuff

--- optimized.c
+++ optimized.c
@@ -1,13 +1,15 @@
+#include <immintrin.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include "get_time.h"
-#include <immintrin.h>
 
 // positive or negative
-typedef char Chip;
+typedef short Chip;
 // 0 or 0xff (=> 1)
-typedef char Bit;
+typedef short Bit;
+#define BIT_0 0
+#define BIT_1 0xffff
 
 #define SATELITE_COUNT 24
 #define CHIP_SEQ_LEN 1023
@@ -22,7 +24,18 @@ void print256(__m256i value)
 {
     const size_t n = 32;
     char buffer[n];
-    _mm256_storeu_si256((__m256i*)buffer, value);
+    _mm256_storeu_si256((__m256i *)buffer, value);
+    for (size_t i = 0; i < n; i++)
+        printf("%d, ", buffer[i]);
+
+    printf("\n");
+}
+
+void print256_16(__m256i value)
+{
+    const size_t n = 16;
+    short buffer[n];
+    _mm256_storeu_si256((__m256i *)buffer, value);
     for (size_t i = 0; i < n; i++)
         printf("%d, ", buffer[i]);
 
@@ -73,13 +86,13 @@ void generate_pseudo_random_sequence(int first_xored_bit, int second_xored_bit,
 
     Bit *it = seq_out;
 
-    for (size_t i = 0; i < 1023; i++)
+    for (size_t i = 0; i < CHIP_SEQ_LEN; i++)
     {
         // write value
         int out_bool = (reg1 & 1) ^ ((reg2 >> (10 - first_xored_bit)) & 1) ^
-                  ((reg2 >> (10 - second_xored_bit)) & 1);
+                       ((reg2 >> (10 - second_xored_bit)) & 1);
 
-        *(it++) = out_bool == 1 ? 0xff : 0;
+        *(it++) = out_bool == 1 ? BIT_1 : BIT_0;
 
         // update first register
         int tmp = (reg1 & 1) ^ ((reg1 & 0x80) >> 7);
@@ -97,33 +110,39 @@ void generate_pseudo_random_sequence(int first_xored_bit, int second_xored_bit,
 void check_satelite_sequence(int satelite_nr, const Chip *chip_seq,
                              const Bit *sat_seq)
 {
-    for (size_t offset = 0; offset < 1023; offset++)
+    // TODO: one extra
+    for (size_t offset = 0; offset < CHIP_SEQ_LEN; offset++)
     {
-        __m256i sum256 = _mm256_setzero_si256();
-        int sum = 0;
+        __m256i acc = _mm256_setzero_si256();
 
-        for (size_t i = 0; i < 1023; i += 32) {
+        for (size_t i = 0; i < CHIP_SEQ_LEN; i += 16)
+        {
             size_t idx = offset + i;
 
             __m256i a = _mm256_lddqu_si256(chip_seq + idx);
             __m256i b = _mm256_lddqu_si256(sat_seq + i);
             __m256i res = _mm256_and_si256(a, b);
 
-            sum256 = _mm256_add_epi8(sum256, res);
+            acc = _mm256_adds_epi16(acc, res);
         }
 
-        char buf[32];
-        _mm256_store_si256(buf, sum256);
+        __m256i hadded2 = _mm256_hadds_epi16(
+            _mm256_hadds_epi16(
+                _mm256_hadds_epi16(acc, _mm256_setzero_si256()),
+                _mm256_setzero_si256()),
+            _mm256_setzero_si256());
 
-        for (int i = 0; i < 32; i++) {
-            sum += (int)buf[i];
-        }
+        short buf[16];
+        _mm256_store_si256(buf, hadded2);
 
-        if (abs(sum) > 200)
+        int sum1 = buf[0] + buf[8];
+
+        if (abs(sum1) > 200)
         {
-            int bit = sum > 0 ? 1 : 0;
+            int bit = sum1 > 0 ? 1 : 0;
             printf("Satellite %2d has sent bit %d (delta = %jd)\n", satelite_nr,
-                bit, offset);
+                   bit, offset);
+
             break;
         }
     }
@@ -145,13 +164,12 @@ int main(int argc, const char **argv)
             exit(1);
         }
 
-
         Chip chip_seq[CHIP_SEQ_LEN * 2];
 
         read_chip_sequence(f, chip_seq);
 
         // duplicate to save modulo operation
-        memcpy(chip_seq + 1023, chip_seq, CHIP_SEQ_LEN);
+        memcpy(chip_seq + CHIP_SEQ_LEN, chip_seq, CHIP_SEQ_LEN * sizeof(Chip));
 
         double time = get_time();
 
